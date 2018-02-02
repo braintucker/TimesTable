5c967e01d7438c034c66f5c18362dd084e009d10
Brain Tucker <brian.briantucker@gmail.com>
2018-02-02 13:30:44 -0600

    seekbar is displayed with values 1-20, value is currently logged in console

--- app/src/main/java/com/briantucker/timestable/MainActivity.java
+++ app/src/main/java/com/briantucker/timestable/MainActivity.java
@@ -2,12 +2,54 @@ package com.briantucker.timestable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timesTableSeekBar = (SeekBar)findViewById(R.id.timesTableSeekBar);
+        ListView timesTableListView = (ListView)findViewById(R.id.timesTableListView);
+
+        timesTableSeekBar.setMax(20);
+        timesTableSeekBar.setProgress(10);
+
+        timesTableSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                int min = 1;
+                int timesTable ;
+
+                if (i < min ){
+
+                    timesTable = min;
+
+                }
+                else {
+                    timesTable = i;
+                }
+
+                Log.i("Seekbar value", Integer.toString(timesTable));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
